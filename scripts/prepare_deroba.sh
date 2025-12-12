4d64a8d1f9a21e828b7649e4934038ed01aafce6
Michall00 <01178589@pw.edu.pl>
2025-12-12 10:49:52 +0100

    feat: add shell scripts for preparing artefacts datasets

---
+++ scripts/prepare_deroba.sh
@@ -0,0 +1,5 @@
+uv run mobile_sam/prepare_deroba.py \
+  --deroba_root /home/msadowski/Studia/Inzynierka/MobileSAM/dataset/DEROBA/DEROBA \
+  --ann_json /home/msadowski/Studia/Inzynierka/MobileSAM/dataset/DEROBA/annotations/DEROBA_val_v2.json \
+  --out_dir /home/msadowski/Studia/Inzynierka/MobileSAM/dataset/prepared_deroba_test \
+  --seed 42
